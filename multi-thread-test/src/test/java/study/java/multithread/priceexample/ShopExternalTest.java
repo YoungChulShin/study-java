efe9369c6ca00fa9190fed3d20f21744ba18b028
YoungChulShin <go1323@gmail.com>
2022-09-22 07:51:31 +0900

    findPrices를 병렬스트림 사용

--- multi-thread-test/src/test/java/study/java/multithread/priceexample/ShopExternalTest.java
+++ multi-thread-test/src/test/java/study/java/multithread/priceexample/ShopExternalTest.java
@@ -18,7 +18,33 @@ class ShopExternalTest {
     String product = "iphone14";
     List<Shop04External> shops = getShops();
     List<String> findProducts = shops.stream()
-        .map(s -> String.format("%s price is %.2f", s.getName(), s.getPrice(product)))
+        .map(s -> {
+          System.out.println(Thread.currentThread().getName());
+          return String.format("%s price is %.2f", s.getName(), s.getPrice(product));
+        })
+        .toList();
+    System.out.println(findProducts);
+
+    System.out.println("[" + LocalDateTime.now() + "] 프로그램 종료 - " + threadName);
+    System.out.println("[" + LocalDateTime.now() + "] 총 시간 - " + (System.currentTimeMillis() - startTime));
+  }
+
+  @Test
+  void findPricesParallel() {
+    String threadName = Thread.currentThread().getName();
+    long startTime = System.currentTimeMillis();
+
+    System.out.println("[" + LocalDateTime.now() + "] 프로그램 시작 - " + threadName);
+
+    String product = "iphone14";
+    List<Shop04External> shops = getShops();
+    // parallelStream을 이용하면 포크조인풀을 이용해서 작업이 수행
+    // 첫번째 작업은 메인스레드에서 실행되고, 나머지 작업은 포크조인풀
+    List<String> findProducts = shops.parallelStream()
+        .map(s -> {
+          System.out.println(Thread.currentThread().getName());
+          return String.format("%s price is %.2f", s.getName(), s.getPrice(product));
+        })
         .toList();
     System.out.println(findProducts);
 
