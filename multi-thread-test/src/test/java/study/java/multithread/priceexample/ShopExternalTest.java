eb65b5ff7ecb68e270e67cfa3272a4d2af7ed31a
YoungChulShin <go1323@gmail.com>
2022-09-22 08:40:16 +0900

    findPrices를 CompletableFuture 사용

--- multi-thread-test/src/test/java/study/java/multithread/priceexample/ShopExternalTest.java
+++ multi-thread-test/src/test/java/study/java/multithread/priceexample/ShopExternalTest.java
@@ -3,6 +3,7 @@ package study.java.multithread.priceexample;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
@@ -46,6 +47,32 @@ class ShopExternalTest {
           return String.format("%s price is %.2f", s.getName(), s.getPrice(product));
         })
         .toList();
+    System.out.println(findProducts);
+
+    System.out.println("[" + LocalDateTime.now() + "] 프로그램 종료 - " + threadName);
+    System.out.println("[" + LocalDateTime.now() + "] 총 시간 - " + (System.currentTimeMillis() - startTime));
+  }
+
+  @Test
+  void findPricesCompletableFuture() {
+    String threadName = Thread.currentThread().getName();
+    long startTime = System.currentTimeMillis();
+
+    System.out.println("[" + LocalDateTime.now() + "] 프로그램 시작 - " + threadName);
+
+    String product = "iphone14";
+    List<Shop04External> shops = getShops();
+    // parallelStream을 이용하면 포크조인풀을 이용해서 작업이 수행
+    // 첫번째 작업은 메인스레드에서 실행되고, 나머지 작업은 포크조인풀
+    List<CompletableFuture<String>> futureFindProducts = shops.stream()
+        .map(s -> CompletableFuture.supplyAsync(
+            () -> {
+              System.out.println(Thread.currentThread().getName());
+              return String.format("%s price is %.2f", s.getName(), s.getPrice(product));
+            }))
+        .toList();
+
+    List<String> findProducts = futureFindProducts.stream().map(CompletableFuture::join).toList();
     System.out.println(findProducts);
 
     System.out.println("[" + LocalDateTime.now() + "] 프로그램 종료 - " + threadName);
