1db00b8100372999df529f12afa6ce775fdbbcb8
YoungChulShin <go1323@gmail.com>
2022-10-13 19:46:06 +0900

    java 10 특징

---
+++ java-version/src/main/java/study/java/version/java10/varEx.java
@@ -0,0 +1,15 @@
+package study.java.version.java10;
+
+public class varEx {
+
+  public static void main(String[] args) {
+    //=============================================
+    // var 키워드
+    //=============================================
+    // java 8
+    String greeting = "hello";
+
+    // java 10
+    var greeting2 = "안녕하세요";  // class java.lang.String
+  }
+}
