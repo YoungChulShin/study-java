aa062bc083542388073c5ab971bd5b34f313dabe
YoungChulShin <go1323@gmail.com>
2022-12-01 23:37:04 +0900

    gradle main class 추가

---
+++ gc-maker/src/main/java/study/gc/DataContainer.java
@@ -0,0 +1,12 @@
+package study.gc;
+
+public class DataContainer {
+
+    private final String word;
+    private final Integer number;
+
+    public DataContainer(String word, Integer number) {
+        this.word = word;
+        this.number = number;
+    }
+}
--- gc-maker/src/main/java/study/gc/GCMaker.java
+++ gc-maker/src/main/java/study/gc/GCMaker.java
@@ -7,9 +7,9 @@ public class GCMaker {
 
     public static void main(String[] args) throws InterruptedException {
         GCMaker gcMaker = new GCMaker();
-        for (int loop = 0; loop < 120; loop++) {
+        for (int loop = 0; loop < 12000; loop++) {
             gcMaker.makeObject();
-            Thread.sleep(1000);
+            Thread.sleep(10);
             System.out.println(".");
         }
     }
@@ -17,7 +17,7 @@ public class GCMaker {
     private void makeObject() {
         Integer[] intArr = new Integer[1024000];
         List<Integer> list = new ArrayList<>(1024000);
-        for (int loop = 0; loop < 1024; loop++) {
+        for (int loop = 0; loop < 100024; loop++) {
             intArr[loop] = loop;
             list.add(loop);
         }
